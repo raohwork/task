283ea334b5763558f85134c8aa6557a363a94e9a
Ronmi Ren <ronmi.ren@gmail.com>
2024-04-09 14:45:32 +0800

    update api of loosy.Notifier/Waiter, to be more specific.

--- lossy/notify.go
+++ lossy/notify.go
@@ -4,6 +4,8 @@
 
 package lossy
 
+import "context"
+
 type notifier struct {
 	ch chan chan struct{}
 }
@@ -12,9 +14,9 @@ type notifier struct {
 //
 // It provides a lossy wait/notify implementation.
 //
-// They shares a channel, which is closed by [Notifier.N]. After the channel is
-// closed, a new channel is shared instead, so further notification is lost before
-// you wait again.
+// They shares a channel, which is closed by [Notifier.Notify]. After the channel
+// is closed, a new channel is shared instead, so further notification is lost
+// before you wait again.
 func NewNotifier() (Notifier, Waiter) {
 	ret := &notifier{
 		ch: make(chan chan struct{}, 1),
@@ -27,32 +29,59 @@ func NewNotifier() (Notifier, Waiter) {
 //
 // It notifies current waiters, and prepares for next notification.
 //
-// See [Waiter.W] for detailed info.
+// See [Waiter.Wait] for detailed info.
 type Notifier interface {
 	// Notifies current waiting waiters.
+	Notify()
+	// Deprecated: Use Notify() which is identical.
 	N()
 }
 
 // Waiter is a channel-bases lossy waiter, coupled with [Notifier].
 //
-// It receives only one notification once you call W().
+// It receives only one notification once you call Wait().
 type Waiter interface {
 	// Waits for next notification. Returned channel is closed by next
-	// Notifier.N call. You'll lose further notifications before you
-	// wait again.
+	// Notifier.Notify call. You'll lose further notifications before
+	// you wait again.
+	Wait() <-chan struct{}
+	// Deprecated: Use Wait() which is identical.
 	W() <-chan struct{}
+
+	// A cancellable wait.
+	WaitCtx(context.Context) error
 }
 
-// N closes current channel, and creates new one for new waiter.
-func (n *notifier) N() {
+// Notify closes current channel, and creates new one for new waiter.
+func (n *notifier) Notify() {
 	ch := <-n.ch
 	close(ch)
 	n.ch <- make(chan struct{})
 }
 
-// W retrieves current channel, which will be closed by next Notify.
-func (n *notifier) W() <-chan struct{} {
+// Wait retrieves current channel, which will be closed by next Notify.
+func (n *notifier) Wait() <-chan struct{} {
 	ret := <-n.ch
 	n.ch <- ret
 	return ret
+}
+
+// N is identical to Notify.
+//
+// Deprecated: Use Notify() instead.
+func (n *notifier) N() { n.Notify() }
+
+// W is identical to Wait.
+//
+// Deprecated: Use Wait() instead.
+func (n *notifier) W() <-chan struct{} { return n.Wait() }
+
+// WaitCtx waits current channel until ctx is canceled.
+func (n *notifier) WaitCtx(ctx context.Context) error {
+	select {
+	case <-n.Wait():
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
 }
