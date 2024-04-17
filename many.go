d6c9c062f38551e7d43e7311c88d9d310f9cca12
Ronmi Ren <ronmi.ren@gmail.com>
2024-04-17 16:57:03 +0800

    First and Skip now supports cancel cause

--- many.go
+++ many.go
@@ -4,7 +4,10 @@
 
 package task
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 // Iter creates a task run tasks with same context and stops at first error.
 func Iter(tasks ...Task) Task {
@@ -19,13 +22,15 @@ func Iter(tasks ...Task) Task {
 	}
 }
 
+var ErrOneHasDone = errors.New("another task has been done")
+
 // First creates a task that runs tasks concurrently, return first result and cancel
-// others.
+// others. Other tasks receives ErrOneHasDone as cancel cause.
 //
 // Take care of [Tiny] tasks as it cannot be cancelled by context.
 func First(tasks ...Task) Task {
 	return func(ctx context.Context) (err error) {
-		ctx, cancel := context.WithCancel(ctx)
+		ctx, cancel := context.WithCancelCause(ctx)
 
 		ch := make(chan error)
 		for _, t := range tasks {
@@ -33,7 +38,7 @@ func First(tasks ...Task) Task {
 		}
 
 		err = <-ch
-		cancel()
+		cancel(ErrOneHasDone)
 		go func() {
 			for i := 1; i < len(tasks); i++ {
 				<-ch
@@ -64,14 +69,28 @@ func Wait(tasks ...Task) Task {
 	}
 }
 
+type ErrOthers struct {
+	cause error
+}
+
+func (e ErrOthers) Error() string {
+	return "canceled by error from other task: " + e.cause.Error()
+}
+
+func (e ErrOthers) Unwrap() error         { return e.cause }
+func (e ErrOthers) Is(err error) bool     { return errors.Is(err, e.cause) }
+func (e ErrOthers) As(v interface{}) bool { return errors.As(e.cause, v) }
+
 // Skip creates a task that runs tasks concurrently, cancel others if any error, and
 // wait them done.
+//
+// Tasks canceled by this receieves ErrOthers which wraps the error as cancel cause.
 //
 // Take care of [Tiny] tasks as it cannot be cancelled by context.
 func Skip(tasks ...Task) Task {
 	return func(ctx context.Context) (err error) {
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
+		ctx, cancel := context.WithCancelCause(ctx)
+		defer cancel(nil)
 
 		ch := make(chan error)
 		for _, t := range tasks {
@@ -82,7 +101,7 @@ func Skip(tasks ...Task) Task {
 			e := <-ch
 			if err == nil && e != nil {
 				err = e
-				cancel()
+				cancel(ErrOthers{e})
 			}
 		}
 
