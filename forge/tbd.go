77199a3a3218c77bef01543bdc560339177680c6
Ronmi Ren <ronmi.ren@gmail.com>
2024-04-25 16:54:29 +0800

    improve performance of forge.Generator.TBD a little; fix typo in doc

--- forge/tbd.go
+++ forge/tbd.go
@@ -11,29 +11,32 @@ import (
 	"github.com/raohwork/task/tbd"
 )
 
-type _bindTBD[T any] struct {
-	g    Generator[T]
-	once sync.Once
-	*tbd.GeneralImpl[T]
-}
-
-func (b *_bindTBD[T]) Get(ctx context.Context) (T, error) {
-	b.once.Do(func() {
-		b.GeneralImpl.Determine(b.g.Run(ctx))
-	})
-	return b.GeneralImpl.Get(ctx)
-}
-
 // TBD creates a binded [tbd.TBD] that resolved by g.
 //
-// It's semantically identical to following code, with bettor performance:
+// It's semantically identical to following code, with better performance:
 //
 //	ret, resolve := tbd.Create[T]()
 //	return tbd.Bind(ret, func(ctx context.Context) error {
 //		return resolve(g.Run(ctx))
 //	}
 func (g Generator[T]) TBD() tbd.TBD[T] {
-	return &_bindTBD[T]{g: g, GeneralImpl: tbd.CreateImpl[T]()}
+	return &asTBD[T]{g: g, ch: make(chan struct{})}
+}
+
+type asTBD[T any] struct {
+	g    Generator[T]
+	once sync.Once
+	ch   chan struct{}
+	data T
+	err  error
+}
+
+func (t *asTBD[T]) Resolved() <-chan struct{} { return t.ch }
+func (t *asTBD[T]) Get(ctx context.Context) (T, error) {
+	t.once.Do(func() {
+		t.data, t.err = t.g(ctx)
+	})
+	return t.data, t.err
 }
 
 // Cached wraps g to cache the result, and reuse it in later call without running g.
