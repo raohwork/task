855622481fc670e2aa10ac173550766be2fbd2e7
Ronmi Ren <ronmi.ren@gmail.com>
2024-09-30 13:52:34 +0800

    add helper to create "NoCtx" version of multi-converter

--- action/convert_multi.go
+++ action/convert_multi.go
@@ -12,6 +12,11 @@ type Converter2[A, B, O any] func(context.Context, A, B) (O, error)
 // Get2 creates an Converter2, mostly for type converting purpose.
 func Get2[A, B, O any](f func(context.Context, A, B) (O, error)) Converter2[A, B, O] { return f }
 
+// NoCtxGet2 is "NoCtx" version of Get2.
+func NoCtxGet2[A, B, O any](f func(A, B) (O, error)) Converter2[A, B, O] {
+	return func(_ context.Context, a A, b B) (O, error) { return f(a, b) }
+}
+
 // From creates a Converter by currifying c with a [Data].
 func (c Converter2[A, B, O]) From(a Data[A]) Converter[B, O] {
 	return func(ctx context.Context, vb B) (ret O, err error) {
@@ -39,6 +44,11 @@ func Get3[A, B, C, O any](f func(context.Context, A, B, C) (O, error)) Converter
 	return f
 }
 
+// NoCtxGet3 is "NoCtx" version of Get3.
+func NoCtxGet3[A, B, C, O any](f func(A, B, C) (O, error)) Converter3[A, B, C, O] {
+	return func(_ context.Context, a A, b B, c C) (O, error) { return f(a, b, c) }
+}
+
 // From creates a Converter2 by currifying c with a [Data].
 func (c Converter3[A, B, C, O]) From(a Data[A]) Converter2[B, C, O] {
 	return func(ctx context.Context, vb B, vc C) (ret O, err error) {
@@ -64,6 +74,11 @@ type Converter4[A, B, C, D, O any] func(context.Context, A, B, C, D) (O, error)
 // Get4 creates an Converter4, mostly for type converting purpose.
 func Get4[A, B, C, D, O any](f func(context.Context, A, B, C, D) (O, error)) Converter4[A, B, C, D, O] {
 	return f
+}
+
+// NoCtxGet4 is "NoCtx" version of Get4.
+func NoCtxGet4[A, B, C, D, O any](f func(A, B, C, D) (O, error)) Converter4[A, B, C, D, O] {
+	return func(_ context.Context, a A, b B, c C, d D) (O, error) { return f(a, b, c, d) }
 }
 
 // From creates a Converter3 by currifying c with a [Data].
